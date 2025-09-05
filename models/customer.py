d346b4091a7e46c04af8f376bd3fed46752ba576
Elmunoz420 <binrock906@gmail.com>
2025-09-05 15:05:06 -0600

    cambio en customer

--- models/customer.py
+++ models/customer.py
@@ -1,18 +1,23 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP, func
 from sqlalchemy.orm import relationship
 from db import Base
 
 class Customer(Base):
     __tablename__ = "customer"
-    
 
     id = Column(Integer, primary_key=True, index=True)
-    ig_username = Column(String(100)    )
+    ig_username = Column(String(100))
     phone = Column(String(50))
     national_id = Column(String(50))
     email = Column(String(255), unique=True)
     full_name = Column(String(160), nullable=False)
-    created_at = Column(TIMESTAMP)
+
+    # Ahora sí con default de Postgres
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
 
     # 👇 relación inversa para que funcione Order.customer
     orders = relationship("Order", back_populates="customer")
